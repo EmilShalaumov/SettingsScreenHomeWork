e8727f0e2f2890c8545b10d9cb45d7a92507c26f
Emil Shalaumov <emil.shalaumov@gmail.com>
2019-10-24 21:58:37 +0300

    Initial Commit

---
+++ SettingsScreenHomeWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SettingsScreenHomeWork
+//
+//  Created by Эмиль Шалаумов on 24.10.2019.
+//  Copyright © 2019 Emil Shalaumov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
